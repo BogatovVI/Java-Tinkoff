54503f4fbd07ac234ae82efdb33e9387a13f7666
BogatovVI <vladosdydos@mail.ru>
2023-08-10 11:44:35 +0300

    Task3 ready

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void inputData(List<Integer> list){
+    public static void inputData(List<Integer> list) {
 
         File file = new File("input.txt");
         Scanner scanner = null;
@@ -19,10 +19,10 @@ public class Main {
             System.exit(1);
         }
 
-        while(scanner.hasNextLine()) {
+        while (scanner.hasNextLine()) {
             number = scanner.nextInt();
 
-            if (number < 1 || number > 2 * Math.pow(10, 9)){
+            if (number < 1 || number > 100) {
                 System.out.println("Некорректные данные.");
                 break;
             }
@@ -36,12 +36,28 @@ public class Main {
         List<Integer> list = new ArrayList<>();
         Main.inputData(list);
 
-        if (list.size() > 1){
-            System.out.println("Ввели больше одного числа.");
+        if (list.get(0) < 2) {
+            System.out.println("Неправильный ввод данных.");
             System.exit(0);
         }
 
-        System.out.println(Integer.toBinaryString(list.get(0)).length());
+        for (int i = 2; i < list.size() - 2; i++) {
+            if (list.get(i) > list.get(i + 1)) {
+                System.out.println("Неправильный ввод.");
+                System.exit(0);
+            }
+        }
+
+        if (list.get(list.size() - 1) > list.size() - 3) {
+            System.out.println("Неправильный ввод.");
+            System.exit(0);
+        }
+
+        if (list.get(list.size() - 1) < list.get(1)) {
+            System.out.println(list.get(list.size() - 2) - 1);
+        } else {
+            System.out.println(list.get(list.size() - 2) - 2 + list.get(list.get(list.size() - 1) + 1));
+        }
 
     }
 }
